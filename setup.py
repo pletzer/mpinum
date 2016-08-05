7085cc4a718cf225772bc9fd4f7d51a22c4d9e0c
Alex Pletzer <alexander@gokliya.net>
2016-08-05 13:36:08 +1200

    build works

--- setup.py
+++ setup.py
@@ -23,12 +23,13 @@ You can download mpi4py from http://code.google.com/p/mpi4py/
     sys.exit(2)
 
 setup(name = 'pnumpy',
-      version = '1.1.0',
+      version = '1.2.0',
       description = 'A very lightweight implementation of distributed arrays',
       author = 'Alexander Pletzer',
       author_email = 'alexander@gokliya.net',
       url = 'https://github.com/pletzer/pnumpy.git',
       install_requires=['mpi4py>=2.0.0'],
       dependency_links=['https://github.com/mpi4py/mpi4py'],
-      packages = ['src'],
+      package_dir={'pnumpy': 'src'}, # the present directory maps to src 
+      packages = ['pnumpy'],
 )
