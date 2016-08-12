cdd85cea498d902b637fcd567f10740f99b44aab
Alex Pletzer <alexander@gokliya.net>
2016-08-12 15:53:47 +1200

    + 2d test case

--- src/pnLaplacian.py
+++ src/pnLaplacian.py
@@ -167,11 +167,77 @@ def test1d():
 
     # set the input function
     inp = 0.5 * axes[0]**2
-    print('[{0}] inp = {1}'.format(rk, str(inp)))
+    #print('[{0}] inp = {1}'.format(rk, str(inp)))
 
     out = lapl.apply(inp) / hs[0]**2
-    print('[{0}] out = {1}'.format(rk, str(out)))
+    #print('[{0}] out = {1}'.format(rk, str(out)))
+
+    # check sum
+    localChkSum = numpy.sum(out.flat)
+    chksum = numpy.sum(MPI.COMM_WORLD.gather(localChkSum, 0))
+    if rk == 0: 
+        print('test1d check sum = {}'.format(chksum))
+
+
+def test2d():
+
+    import sys
+    from pnCubeDecomp import CubeDecomp
+
+    # number of procs
+    sz = MPI.COMM_WORLD.Get_size()
+    # MPI rank
+    rk = MPI.COMM_WORLD.Get_rank()
+
+    n = 8
+
+    # global number of cells
+    ns = (n, n)
+
+    # domain decomposition
+    dc = CubeDecomp(sz, ns)
+    if not dc.getDecomp():
+        print('*** ERROR Invalid domain decomposition -- rerun with different sizes/number of procs')
+        sys.exit(1)
+    ndims = dc.getNumDims()
+
+    # local start/end grid indices
+    slab = dc.getSlab(rk)
+
+    # global domain boundaries
+    xmins = numpy.array([0.0 for i in range(ndims)])
+    xmaxs = numpy.array([1.0 for i in range(ndims)])
+
+    # local cell centered coordinates
+    axes = []
+    hs = []
+    nsLocal = []
+    for i in range(ndims):
+        ibeg, iend = slab[i].start, slab[i].stop
+        nsLocal.append(iend - ibeg)
+        h = (xmaxs[i] - xmins[i]) / float(ns[i])
+        ax = xmins[i] + h*(numpy.arange(ibeg, iend) + 0.5)
+        hs.append(h)
+        axes.append(ax)
+
+    lapl = Laplacian(dc, periodic=(False, False))
+
+    # set the input function
+    xx = numpy.outer(axes[0], numpy.ones((nsLocal[1],)))
+    yy = numpy.outer(numpy.ones((nsLocal[0],)), axes[1])
+    inp = 0.5 * xx * yy ** 2
+    #print('[{0}] inp = {1}'.format(rk, str(inp)))
+
+    out = lapl.apply(inp) / hs[0]**2
+    #print('[{0}] out = {1}'.format(rk, str(out)))
+
+    # check sum
+    localChkSum = numpy.sum(out.flat)
+    chksum = numpy.sum(MPI.COMM_WORLD.gather(localChkSum, 0))
+    if rk == 0: 
+        print('test2d check sum = {}'.format(chksum))
 
 
 if __name__ == '__main__': 
     test1d()
+    test2d()
